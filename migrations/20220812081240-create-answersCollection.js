72f8ba7c8235cf27960b2fdeffa0d7ebd3da2974
Seongkyu <sinsk23@naver.com>
2022-08-12 21:12:17 +0900

    model,migrations 변수명

--- migrations/20220812081240-create-answersCollection.js
+++ migrations/20220812081240-create-answersCollection.js
@@ -8,7 +8,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER,
       },
-      userId: {
+      userIdNo: {
         allowNull: true,
         type: Sequelize.INTEGER,
       },
--- models/answersCollection.js
+++ models/answersCollection.js
@@ -19,7 +19,7 @@ module.exports = (sequelize, DataTypes) => {
         primaryKey: true,
         type: DataTypes.INTEGER,
       },
-      userId: {
+      userIdNo: {
         allowNull: true,
         type: DataTypes.INTEGER,
       },
