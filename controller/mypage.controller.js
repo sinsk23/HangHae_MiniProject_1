78beeef430fbda323d4224c2ddc273a16815df31
Bohyeon Kim <love.free.work@gmail.com>
2022-08-13 20:49:46 +0900

    authMiddleware 추가'

--- controller/mypage.controller.js
+++ controller/mypage.controller.js
@@ -1,24 +1,13 @@
 const MypageService = require("../services/mypage.service");
 const ResultsRepository = require("../repositories/results.repository");
 
-
-
-
-
-class MypageController{
-    // mypageService = new MypageService();
-    // resultsRepository = new ResultsRepository();
-
-    // bringMypage = async(req, res, next) =>{
-        
-        
-
-    // }
-
-    // bringMyinfo = async(req, res, next) =>{
-        
-    // } 
-
+class MypageController {
+  // mypageService = new MypageService();
+  // resultsRepository = new ResultsRepository();
+  // bringMypage = async(req, res, next) =>{
+  // }
+  // bringMyinfo = async(req, res, next) =>{
+  // }
 }
 
-module.exports= MypageController;
+module.exports = MypageController;
--- routes/mypage.routes.js
+++ routes/mypage.routes.js
@@ -3,12 +3,22 @@ const express = require("express");
 const MypageController = require("../controller/mypage.controller");
 const mypageController = new MypageController();
 
+const AuthController = require("../controller/auth.controller");
+const authController = new AuthController();
+
 const router = express.Router();
 
-
 //마이페이지
-router.get("/mypage", mypageController.bringMypage);
+router.get(
+  "/mypage",
+  authController.authMiddleware,
+  mypageController.bringMypage
+);
 
 //내 결과정보도 불러오기
 //(가장 마지막 추천 결과 GET)
-router.get("/mypage/myanswers", mypageController.bringMyinfo);
+router.get(
+  "/mypage/myanswers",
+  authController.authMiddleware,
+  mypageController.bringMyinfo
+);
