9a9734915583b095abe7a04cea33b7bb0c0c0c25
Bohyeon Kim <love.free.work@gmail.com>
2022-08-15 12:12:36 +0900

    머지 후 자잘한 버그 해결

--- controller/mypage.controller.js
+++ controller/mypage.controller.js
@@ -1,4 +1,3 @@
-
 //결과 저장소에서 함수 필요한거 쓰기
 const ResultsRepository = require("../repositories/results.repository");
 const ResultsService = require("../services/results.service");
@@ -7,7 +6,6 @@ const ResultsService = require("../services/results.service");
 const CountryinfoRepository = require("../repositories/countryInfo.repository");
 
 class MypageController {
-  
   resultsRepository = new ResultsRepository();
   resultsService = new ResultsService();
   countryinfoRepository = new CountryinfoRepository();
@@ -16,16 +14,13 @@ class MypageController {
   bringMypage = async (req, res, next) => {
     try {
       const { userId, nickname } = res.locals.user;
+      console.log(userId, nickname);
       const result = { userId, nickname };
+      return res.status(200).json(result);
+    } catch (err) {
+      return res.status(400).json({ err: err.message });
     }
-    catch{}
-  }
-  
-  
-  
-  
-  
-  
+  };
 
   // 마이페이지에 넣을 나의 저장된 결과지 result 받아서 반환
   bringMyinfo = async (req, res, next) => {
@@ -34,17 +29,15 @@ class MypageController {
       const myResult = await this.resultsRepository.getResultByUserIdNo(_id);
 
       if (myResult) {
-        const result = await this.ResultsService.resultPage(myResult.resultId);
+        const result = await this.resultsService.resultPage(myResult.resultId);
         return res.status(200).json(result);
       } else {
         return res.status(400).json({ message: "설문을 먼저 진행해주세요." });
       }
-
     } catch (err) {
       return res.status(400).json({ err: err.message });
     }
   };
 }
-
 
 module.exports = MypageController;
