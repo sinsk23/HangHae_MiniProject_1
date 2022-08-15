bc210ecc4d10ff519a01893f7c8cd93e1c7acb4a
Bohyeon Kim <love.free.work@gmail.com>
2022-08-15 14:50:20 +0900

    hashFunction 적용하여 비밀번호 암호화

--- repositories/users.repository.js
+++ repositories/users.repository.js
@@ -1,5 +1,6 @@
 // 필요한 시퀄라이즈 모델을 확보합니다.
 const { User } = require("../models");
+const crypto = require("crypto");
 
 // 저장소 클래스를 선언합니다.
 class UserRepository {
@@ -33,9 +34,10 @@ class UserRepository {
 
   // userId와 password 동시에 맞는 1명의 유저를 찾는다.
   getUserbyIdPw = async (userId, password) => {
+    const hashedPassword = this.hashFunction(password);
     // 찾아서
     const userInfo = await User.findOne({
-      where: { userId, password },
+      where: { userId, password: hashedPassword },
     });
 
     // 리턴
@@ -44,11 +46,26 @@ class UserRepository {
 
   // User DB 생성
   createUser = async (userId, nickname, password) => {
-    // 전달받은 인자를 담아 DB에 전달하여 저장합니다.
-    const createUserData = await User.create({ userId, nickname, password });
+    const hashedPassword = this.hashFunction(password);
+
+    // 전달받은 인자와 Hashed 암호를 담아 DB에 전달하여 저장합니다.
+    const createUserData = await User.create({
+      userId,
+      nickname,
+      password: hashedPassword,
+    });
 
     // 방금 생성한 유저 데이터를 리턴
     return createUserData;
+  };
+
+  hashFunction = (password) => {
+    const secret = process.env.MY_SECRET_KEY;
+    const hashedPassword = crypto
+      .createHash("sha256", secret)
+      .update(password)
+      .digest("hex");
+    return hashedPassword;
   };
 }
 
